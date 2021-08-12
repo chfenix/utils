bb79c2f4c64b10c623b4575e6a66c2a3de9c6d23
chfenix <chfenix@163.com>
2021-08-12 13:09:08 +0800

    增加Java8Stream Sample

---
+++ common/src/main/java/cn/solwind/sample/Java8Stream.java
@@ -0,0 +1,86 @@
+package cn.solwind.sample;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Java8Stream {
+
+    public static void main(String[] args) throws Exception {
+        List<Java8StreamDTO> listDTO = init();
+
+        /**
+         * 提取DTO某一属性
+         */
+        System.out.println("提取DTO某一属性");
+        System.out.println(listDTO.stream().map(e -> Long.valueOf(e.getId())).collect(Collectors.toList()));
+        System.out.println();
+
+        /**
+         * 按照某一属性汇总DTO，Map-List
+         */
+        System.out.println("按照某一属性汇总DTO");
+        Map<String, List<Java8StreamDTO>> listMap = listDTO.stream().collect(Collectors.groupingBy(Java8StreamDTO::getGroupKey));
+        System.out.println(listMap);
+        System.out.println();
+
+        /**
+         * 按照某一属性汇总合并DTO，返回合并后List，复合key group可以使用自定义方法获取复合key
+         */
+        System.out.println("按照某一属性汇总");
+        List<Java8StreamDTO> listGroupDTO = listDTO.stream()
+                .collect(Collectors.toMap(k -> groupKey(k), a -> a, (o1, o2) -> {
+                    o1.setAmount(o1.getAmount().add(o2.getAmount()));
+                    o1.setBeginDate(o1.getBeginDate().isAfter(o2.getBeginDate()) ? o2.getBeginDate() : o1.getBeginDate());
+                    o1.setEndDate(o1.getEndDate().isBefore(o2.getEndDate()) ? o2.getEndDate() : o1.getEndDate());
+                    o1.setId(o1.getId() + " - " + o2.getId());
+                    return o1;
+                })).values().stream().collect(Collectors.toList());
+        System.out.println(listGroupDTO);
+        System.out.println();
+        listDTO = init();
+
+        /**
+         * 按照某一属性过滤
+         */
+        System.out.println("按照某一属性过滤");
+        System.out.println(listDTO.stream().filter(t -> t.getBeginDate().isAfter(LocalDate.of(2021,4,15))).collect(Collectors.toList()));
+        System.out.println();
+
+        /**
+         * 按照某一属性转Map，重复的覆盖
+         */
+        System.out.println("按照某一属性转Map，重复的覆盖");
+        Map<String, Java8StreamDTO> listOneMap = listDTO.stream().collect(Collectors.toMap(Java8StreamDTO::getId, a -> a, (k1, k2) -> k1));
+        System.out.println(listOneMap);
+        System.out.println();
+
+    }
+
+    private static List<Java8StreamDTO> init() {
+        Object[][] initData = new Object[][]{
+                {"1", new BigDecimal(1), LocalDate.of(2021, 1, 1), LocalDate.of(2021, 1, 31), "1"},
+                {"2", new BigDecimal(2), LocalDate.of(2021, 2, 1), LocalDate.of(2021, 2, 28), "2"},
+                {"3", new BigDecimal(3), LocalDate.of(2021, 3, 1), LocalDate.of(2021, 3, 31), "1"},
+                {"4", new BigDecimal(4), LocalDate.of(2021, 4, 1), LocalDate.of(2021, 4, 30), "2"},
+                {"5", new BigDecimal(5), LocalDate.of(2021, 5, 1), LocalDate.of(2021, 5, 31), "1"},
+                {"1", new BigDecimal(6), LocalDate.of(2021, 6, 1), LocalDate.of(2021, 6, 30), "2"}
+        };
+
+        List<Java8StreamDTO> listDTO = new ArrayList<>();
+        for (Object[] initDatum : initData) {
+            listDTO.add(new Java8StreamDTO(initDatum));
+        }
+
+        return listDTO;
+    }
+
+    private static String groupKey(Java8StreamDTO java8StreamDTO) {
+        return java8StreamDTO.getGroupKey();
+    }
+}
---
+++ common/src/main/java/cn/solwind/sample/Java8StreamDTO.java
@@ -0,0 +1,76 @@
+package cn.solwind.sample;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Java8StreamDTO {
+
+    private String id;
+
+    private BigDecimal amount;
+
+    private LocalDate beginDate;
+
+    private LocalDate endDate;
+
+    private String groupKey;
+
+    public Java8StreamDTO(Object[] initData) {
+        this.id = (String)initData[0];
+        this.amount = (BigDecimal)initData[1];
+        this.beginDate = (LocalDate)initData[2];
+        this.endDate = (LocalDate)initData[3];
+        this.groupKey = (String)initData[4];
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getBeginDate() {
+        return beginDate;
+    }
+
+    public void setBeginDate(LocalDate beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getGroupKey() {
+        return groupKey;
+    }
+
+    public void setGroupKey(String groupKey) {
+        this.groupKey = groupKey;
+    }
+
+    @Override
+    public String toString() {
+        return "Java8StreamDTO{" +
+                "id='" + id + '\'' +
+                ", amount=" + amount +
+                ", beginDate=" + beginDate +
+                ", endDate=" + endDate +
+                ", groupKey='" + groupKey + '\'' +
+                '}';
+    }
+}
